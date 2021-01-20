f31aa2df648c219da54f29768646e89293b1b4a0
Karla Cervantes <cervantes.karla14@gmail.com>
2021-01-20 00:36:02 -0600

    app not fully functioning yet

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const AddedFeature = props => {
+
+  // const removerHandler =(id)=>{
+  //   props.removeFeature(id);
+  // }
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={() => props.removeFeature({type:'REMOVE_FEATURE', payload: props.feature.price})} className="button">X</button>
       {props.feature.name}
     </li>
   );
